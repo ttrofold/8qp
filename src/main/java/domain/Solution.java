dd659ac9c750e67f9d69df8fc62e29ad4ff72245
antonkazmerchuk <fake@email1>
2012-11-24 21:22:42 +0300

    First tests for Solution.

--- src/main/java/domain/Solution.java
+++ src/main/java/domain/Solution.java
@@ -1,4 +1,20 @@
 package domain;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Solution {
+    private List<Integer> solutionList;
+
+    public Solution() {
+        solutionList = new LinkedList<Integer>();
+    }
+
+    public List<Integer> getSolutionList() {
+        return solutionList;
+    }
+
+    public void augment(int i) {
+        solutionList.add(i);
+    }
 }
--- src/test/java/domain/SolutionTest.java
+++ src/test/java/domain/SolutionTest.java
@@ -1,5 +1,29 @@
 package domain;
 
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
 public class SolutionTest extends AbstractMockTest {
 
+    @Test
+    public void
+    encapsulatesListWithNumericSolution() {
+        assertNotNull(new Solution().getSolutionList());
+    }
+
+    @Test
+    public void
+    canBeAugmentedWithNewValue() {
+        Solution solution = new Solution();
+
+        solution.augment(1);
+
+        List<Integer> solutionList = solution.getSolutionList();
+        assertEquals(1, solutionList.size());
+        assertEquals(Integer.valueOf(1), solutionList.get(0));
+    }
 }
